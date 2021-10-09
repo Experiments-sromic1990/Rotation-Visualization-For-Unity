e90efec2e3fcda892b9c67d224c18c9089c0c4cb
Sourav Chatterjee <sromic@gmail.com>
2021-10-09 22:47:09 +0530

    Added rotation code for each axis

--- Assets/RotationVisualization/Code/Rotation.cs
+++ Assets/RotationVisualization/Code/Rotation.cs
@@ -9,6 +9,7 @@ namespace RotationVisualization
     public class Rotation : MonoBehaviour
     {
         [SerializeField] private float rotationAngle = 45;
+        [SerializeField] private Axis rotationAxis = Axis.Y;
         
         private List<AngleViewBase> _angleViews;
 
@@ -20,19 +21,44 @@ namespace RotationVisualization
         private IEnumerator Start()
         {
             yield return new WaitForSeconds(1f);
-            transform.position = Rotate(rotationAngle, Axis.Y);
+            transform.position = Rotate(rotationAngle, rotationAxis);
         }
 
         private Vector3 Rotate(float angle, Axis axis)
         {
-            var startPosition = transform.position;
-            var position = transform.position;
-            Vector3 initialPos = position;
-            float posX = initialPos.x * Mathf.Cos(angle * Mathf.Rad2Deg) + initialPos.z * Mathf.Sin(angle * Mathf.Rad2Deg);
-            float posZ = initialPos.x * -Mathf.Sin(angle * Mathf.Rad2Deg) + initialPos.z * Mathf.Cos(angle * Mathf.Rad2Deg);
+            Vector3 initialPos = transform.position;
+            Vector3 position = RotateAccordingToAxis(initialPos, angle, axis);
+            UpdateView(initialPos, position);
+            return position;
+        }
 
-            position = new Vector3(posX, position.y, posZ);
-            UpdateView(startPosition, position);
+        private Vector3 RotateAccordingToAxis(Vector3 initialPos, float angle, Axis axis)
+        {
+            Vector3 position = initialPos;
+            float posX = position.x;
+            float posY = position.y;
+            float posZ = position.z;
+            
+            switch (axis)
+            {
+                case Axis.X:
+                    posX = initialPos.x;
+                    posY = initialPos.y * Mathf.Cos(angle * Mathf.Rad2Deg) + initialPos.z * -Mathf.Sin(angle * Mathf.Rad2Deg);
+                    posZ = initialPos.y * Mathf.Sin(angle * Mathf.Rad2Deg) + initialPos.z * Mathf.Cos(angle * Mathf.Rad2Deg);
+                    break;
+                case Axis.Y:
+                    posX = initialPos.x * Mathf.Cos(angle * Mathf.Rad2Deg) + initialPos.z * Mathf.Sin(angle * Mathf.Rad2Deg);
+                    posY = initialPos.y;
+                    posZ = initialPos.x * -Mathf.Sin(angle * Mathf.Rad2Deg) + initialPos.z * Mathf.Cos(angle * Mathf.Rad2Deg);
+                    break;
+                case Axis.Z:
+                    posX = initialPos.x * Mathf.Cos(angle * Mathf.Rad2Deg) + initialPos.y * -Mathf.Sin(angle * Mathf.Rad2Deg);
+                    posY = initialPos.x * Mathf.Sin(angle * Mathf.Rad2Deg) + initialPos.y * Mathf.Cos(angle * Mathf.Rad2Deg);
+                    posZ = initialPos.z;
+                    break;
+            }
+            
+            position = new Vector3(posX, posY, posZ);
             return position;
         }
 
